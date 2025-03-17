7dd7addf5c29869bade2f2b83b95b27b2153bcfd
Satyabrata_das <150060376+princeXsatya@users.noreply.github.com>
2025-03-17 23:50:34 +0530

    Update DatabaseConfig.java

--- Real Time Crime Reporting System/src/main/java/com/CrimeReport/config/DatabaseConfig.java
+++ Real Time Crime Reporting System/src/main/java/com/CrimeReport/config/DatabaseConfig.java
@@ -6,8 +6,8 @@ import java.sql.SQLException;
 
 public class DatabaseConfig {
     private static final String URL = "jdbc:mysql:///crime_reporting_dbs";
-    private static final String USER = "root";
-    private static final String PASSWORD = "1234";
+    private static final String USER = "database username";
+    private static final String PASSWORD = "database password";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
